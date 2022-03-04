35b71bcd2c10d45c9725c05b864fcb997bf909ac
Qin Liu <57234075+qinliuliuqin@users.noreply.github.com>
2022-03-03 21:20:29 -0500

    Delete run_demo.sh

--- run_demo.sh
+++
@@ -1,8 +0,0 @@
-model_folder=/playpen-raid2/qinliu/projects/iSegFormer/weights
-#model_name=cocolvis_hr32_epoch_54.pth
-#model_name=oaizib_swin_base_epoch_54.pth
-model_name=oai_pretrain_oaizib_finetune_hr32_epoch_54.pth
-
-python3 demo.py \
---checkpoint=${model_folder}/${model_name} \
---gpu 1
