4f8b8059bfb816df971bb0eda87106d32ee6d07f
Qin Liu <qinliu2020@gmail.com>
2022-09-25 06:34:24 -0400

    Update

--- evaluate.sh
+++ evaluate.sh
@@ -6,11 +6,22 @@
 # MODEL_PATH=/playpen-raid2/qinliu/models/model_0915_2022/iter_mask/cocolvis_plainvit_base448/001/checkpoints/last_checkpoint.pth
 # MODEL_PATH=/playpen-raid2/qinliu/models/model_0916_2022/iter_mask/sbd_plainvit_base448/008_lr_adjust/checkpoints/054.pth
 # MODEL_PATH=/playpen-raid2/qinliu/models/model_0916_2022/iter_mask/sbd_plainvit_base448/016_lr_adjust/checkpoints/054.pth
-MODEL_PATH=/playpen-raid2/qinliu/models/model_0916_2022/iter_mask/sbd_plainvit_base448/015_upsample_x4/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0916_2022/iter_mask/sbd_plainvit_base448/015_upsample_x4/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0920_2022/iter_mask/sbd_plainvit_base448/002_sbd_baseline/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0920_2022/iter_mask/sbd_plainvit_base448/004_sbd_random_split/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0920_2022/iter_mask/cocolvis_plainvit_base448/000_cocolvis_baseline/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0921_2022/iter_mask/cocolvis_plainvit_base448/000_cocolvis_base_230epochs/checkpoints/150.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0922_2022/iter_mask/sbd_plainvit_base448/002_plainvit_base448_sbd_clicks_encoding_change/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0922_2022/iter_mask/sbd_plainvit_base448/005/checkpoints/last_checkpoint.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0923_2022/iter_mask/sbd_plainvit_base448/000_lr_1e-4/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0923_2022/iter_mask/cocolvis_plainvit_base448/000_base448_cocolvis_lr_5e-5/checkpoints/053.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0923_2022/iter_mask/cocolvis_vit_large448/002_cocolvis_vit_large448/checkpoints/054.pth
+# MODEL_PATH=/playpen-raid2/qinliu/models/model_0923_2022/iter_mask/sbd_vit_large448/000_sbd_vit_large448/checkpoints/054.pth
+MODEL_PATH=/playpen-raid2/qinliu/projects/iSegFormer/weights/cocolvis_vitl_epoch_54.pth
 
 python scripts/evaluate_model.py NoBRS \
---gpu=1 \
+--gpu=0 \
 --checkpoint=${MODEL_PATH} \
---eval-mode=fixed448
-# --datasets=OAIZIB,COCO_MVal
+--eval-mode=cvpr
+# --datasets=DAVIS
 # --vis-preds
