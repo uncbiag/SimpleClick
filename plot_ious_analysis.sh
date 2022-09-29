f42a85ca2d7bea57f031ee46d822e2d3d0d62944
Qin Liu <qinliu2020@gmail.com>
2022-09-29 13:38:40 -0400

    Modify the format

--- plot_ious_analysis.sh
+++ plot_ious_analysis.sh
@@ -1,7 +1,13 @@
-model1=/playpen-raid2/qinliu/models/model_0927_2022/evaluation_logs/others/sbd_vitb_epoch_54
-model2=/playpen-raid2/qinliu/models/model_0927_2022/evaluation_logs/others/sbd_vitl_epoch_54
-model3=/playpen-raid2/qinliu/models/model_0927_2022/evaluation_logs/others/sbd_vith_epoch_54
+root=/playpen-raid2/qinliu/models/model_0928_2022/evaluation_logs/others
+sbd_vitb_grabcut=${root}/sbd_vitb_epoch_54/plots/GrabCut_cvpr_NoBRS_20.pickle
+sbd_vitl_grabcut=${root}/sbd_vitl_epoch_54/plots/GrabCut_cvpr_NoBRS_20.pickle
+sbd_vith_grabcut=${root}/sbd_vith_epoch_54/plots/GrabCut_cvpr_NoBRS_20.pickle
+# cocolvis_vitb=${root}/cocolvis_vitb_epoch_54
+# cocolvis_vitl=${root}/cocolvis_vitl_epoch_54
+# # cocolvis_vith=${root}/cocolvis_vith_epoch_54
+# cocolvis_vith=${root}/052
 
 python ./scripts/plot_ious_analysis.py \
-  --model-dirs ${model1} ${model2} ${model3} \
-  --mode NoBRS
+  --files ${sbd_vitb_grabcut} ${sbd_vitl_grabcut} ${sbd_vith_grabcut} \
+  --mode NoBRS \
+  --datasets GrabCut
