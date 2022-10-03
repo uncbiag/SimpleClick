7ef74279fe5e77fdf242c59d3119e41e78c68219
Qin Liu <qinliu2020@gmail.com>
2022-10-02 21:45:18 -0400

    Delete unnecessary files

--- maskprop/MiVOS/dataset/onehot_util.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def all_to_onehot(masks, labels):
-    Ms = np.zeros((len(labels), masks.shape[0], masks.shape[1], masks.shape[2]), dtype=np.uint8)
-    for k, l in enumerate(labels):
-        Ms[k] = (masks == l).astype(np.uint8)
-    return Ms
