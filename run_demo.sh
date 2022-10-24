6a50b606dffdfafee8dcaa28c7420d590cf8b41f
Qin Liu <qinliu2020@gmail.com>
2022-10-24 10:45:04 -0400

    Fix a bug in demo.sh

--- run_demo.sh
+++ run_demo.sh
@@ -1,6 +1,17 @@
-model_folder=./weights
-model_name=cocolvis_vith_epoch_52.pth
+# Run demo for SimpleClick models
+model_folder=./weights/simpleclick_models
+model_name=cocolvis_vitl_epoch_54.pth
 
 python3 demo.py \
 --checkpoint=${model_folder}/${model_name} \
 --gpu 0
+
+
+# # Run demo for RITM models
+# model_folder=./weights/ritm_models
+# model_name=coco_lvis_h32_itermask.pth
+
+# python3 demo.py \
+# --checkpoint=${model_folder}/${model_name} \
+# --gpu 0 \
+# --eval-ritm 
