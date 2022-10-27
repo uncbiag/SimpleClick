8f66cbfbe0cab605c7c4f6b1d8ac83eadb41a88b
Qin Liu <qinliu2020@gmail.com>
2022-10-27 10:50:47 -0400

    Add comments for running docker. Change Xauthority-qinliu19 to your file, otherwise the GUI may fail

--- docker/cuda11.0/run_docker.sh
+++ docker/cuda11.0/run_docker.sh
@@ -1,8 +1,8 @@
 docker run -e DISPLAY=$DISPLAY \
            --name="qin_simpleclick_dev" \
-           -v /tmp/Xauthority-qinliu19:/root/.Xauthority:rw \
+           -v /tmp/Xauthority-qinliu19:/root/.Xauthority:rw \ # Change to your Xautority file
            --shm-size 16G \
            --gpus all \
-           -v /playpen-raid2/qinliu/data:/work/data \
-           -v /playpen-raid2/qinliu/projects:/work/projects \
+           -v /playpen-raid2/qinliu/data:/work/data \ # Mount your data folder
+           -v /playpen-raid2/qinliu/projects:/work/projects \  # Mount your code folder
            --net host --rm -it simpleclick:cu110_v1.0 bash
