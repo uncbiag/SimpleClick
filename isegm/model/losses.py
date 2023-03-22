3e4c4be44da4183828b2d393d50c41edb254dd38
Qin Liu <qinliu2020@gmail.com>
2023-03-22 14:48:07 -0400

    Add Dice Loss

--- isegm/model/losses.py
+++ isegm/model/losses.py
@@ -159,3 +159,37 @@ class SigmoidBinaryCrossEntropyLoss(nn.Module):
 
         loss = self._weight * (loss * sample_weight)
         return torch.mean(loss, dim=misc.get_dims_with_exclusion(loss.dim(), self._batch_axis))
+
+
+class BinaryDiceLoss(nn.Module):
+    """ Dice Loss for binary segmentation
+    """
+
+    def forward(self, pred, label):
+        batchsize = pred.size(0)
+
+        # convert probability to binary label using maximum probability
+        input_pred, input_label = pred.max(1)
+        input_pred *= input_label.float()
+
+        # convert to floats
+        input_pred = input_pred.float()
+        target_label = label.float()
+
+        # convert to 1D
+        input_pred = input_pred.view(batchsize, -1)
+        target_label = target_label.view(batchsize, -1)
+
+        # compute dice score
+        intersect = torch.sum(input_pred * target_label, 1)
+        input_area = torch.sum(input_pred * input_pred, 1)
+        target_area = torch.sum(target_label * target_label, 1)
+
+        sum = input_area + target_area
+        epsilon = torch.tensor(1e-6)
+
+        # batch dice loss and ignore dice loss where target area = 0
+        batch_loss = torch.tensor(1.0) - (torch.tensor(2.0) * intersect + epsilon) / (sum + epsilon)
+        loss = batch_loss.mean()
+
+        return loss
