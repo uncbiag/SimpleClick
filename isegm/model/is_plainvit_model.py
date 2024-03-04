070109276a5784445e24dd651a553b0a9cfb491f
guba <yukai.gu@outlook.com>
2024-03-04 22:57:35 +0800

    ADD: add initial files for modification.

--- isegm/model/is_plainvit_model.py
+++ isegm/model/is_plainvit_model.py
@@ -93,3 +93,42 @@ class PlainVitModel(ISModel):
         multi_scale_features = self.neck(backbone_features)
 
         return {'instances': self.head(multi_scale_features), 'instances_aux': None}
+
+class MultiOutVitModel(ISModel):
+    @serialize
+    def __init__(
+        self,
+        backbone_params={},
+        neck_params={},
+        head_params={},
+        random_split=False,
+        **kwargs
+        ):
+
+        super().__init__(**kwargs)
+        self.random_split = random_split
+
+        self.patch_embed_coords = PatchEmbed(
+            img_size= backbone_params['img_size'],
+            patch_size=backbone_params['patch_size'],
+            in_chans=3 if self.with_prev_mask else 2,
+            embed_dim=backbone_params['embed_dim'],
+        )
+
+        self.backbone = VisionTransformer(**backbone_params)
+        self.neck = SimpleFPN(**neck_params)
+        self.head = SwinTransfomerSegHead(**head_params)
+        self.testMsg = "Test Message"
+
+    def backbone_forward(self, image, coord_features=None):
+        coord_features = self.patch_embed_coords(coord_features)
+        backbone_features = self.backbone.forward_backbone(image, coord_features, self.random_split)
+
+        # Extract 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        B, N, C = backbone_features.shape
+        grid_size = self.backbone.patch_embed.grid_size
+
+        backbone_features = backbone_features.transpose(-1,-2).view(B, C, grid_size[0], grid_size[1])
+        multi_scale_features = self.neck(backbone_features)
+
+        return {'instances': self.head(multi_scale_features), 'instances_aux': None}
